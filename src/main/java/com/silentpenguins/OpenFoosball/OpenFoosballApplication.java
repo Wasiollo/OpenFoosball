358c071a60b98f257b956b0c8a58471cf4cb97ad
Piotr Komorowski <pjpkomorowski@gmail.com>
2018-04-21 17:46:56 +0200

    Reorganize project from scratch (springboot)

---
+++ src/main/java/com/silentpenguins/OpenFoosball/OpenFoosballApplication.java
@@ -0,0 +1,12 @@
+package com.silentpenguins.OpenFoosball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFoosballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenFoosballApplication.class, args);
+	}
+}

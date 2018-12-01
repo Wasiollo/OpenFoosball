89917294d4be31f00f8ce3bea0a9438102444be6
mwasiak <mwasiak@bluesoft.net.pl>
2018-12-01 21:34:27 +0100

    Spring boot startup

---
+++ backend/src/main/java/com/silent/penguin/OpenFoosball/OpenFoosballApplication.java
@@ -0,0 +1,12 @@
+package com.silent.penguin.OpenFoosball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFoosballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenFoosballApplication.class, args);
+	}
+}
